67231d54419aa14b90ab34c5a5b40ef5ad7e26c1
BENONI ESCKINDER <besckinder@gmail.com>
2024-10-09 22:28:32 +0300

    Button color issues on home page fixed

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
       {/* Popular Pizzas */}
       <section className="px-3 py-6">
         <h3 className="text-gray3 text-xl md:text-2xl">Popular pizzas</h3>
-        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 place-items-center">
+        <div className="grid place-items-center gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           {pizzas.length > 0 &&
             pizzas.map((pizza) => <Pizza key={pizza.id} pizza={pizza} />)}
         </div>
@@ -79,7 +79,7 @@ export default function Home() {
       {/* Fasting Pizzas */}
       <section className="px-3 py-6">
         <h3 className="text-gray3 text-xl md:text-2xl">Fasting pizzas</h3>
-        <div className="flex justify-start gap-4 overflow-x-auto">
+        <div className=" ml-3 flex justify-start gap-4 overflow-x-auto">
           {pizzas.length > 0 &&
             pizzas.map((pizza) => <Pizza key={pizza.id} pizza={pizza} />)}
         </div>
