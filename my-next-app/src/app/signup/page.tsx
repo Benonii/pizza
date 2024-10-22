0993f75fd5396e3619a73b404b9347593f63d3aa
BENONI ESCKINDER <you@example.com>
2024-10-22 22:10:49 +0300

    Input validation for /signup complete

--- my-next-app/src/app/signup/page.tsx
+++ my-next-app/src/app/signup/page.tsx
@@ -7,6 +7,7 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { z } from "zod";
 
 import pizzaBanner from '@/../public/assets/images/pizza-banner.jpg';
 import pizzaIcon from '@/../public/assets/images/pizza-icon.png'
@@ -20,11 +21,27 @@ function Page() {
     phoneNumber: '',
     confirmPassword: '',
   });
-  const [error, setError] = useState<string | null>(null);
+  const [errors, setErrors] = useState<{[key: string]: string}>({});
   const [success, setSuccess] = useState<string | null>(null);
+  const [ termsAccepted, setTermsAccepted ] = useState<boolean>(false);
 
   const label = { inputProps: { 'aria-label': 'Terms and conditions' } };
   const router = useRouter();
+
+  const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().regex(/[a-z]/, {message: "Password must contain at least one lowercase letter"})
+                        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+                        .regex(/[0-9]/, { message: "Password must contain at least one number" })
+                        .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: "Password must contain at least one special character" })
+                        .min(8, 'Password must be at least 8 characters long'),
+    confirmPassword: z.string(),
+    location: z.string().min(2, 'Location too short'),
+    phoneNumber: z.string().min(10, 'Phone number too short').max(10, 'Phone number too long'),
+  }).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  })
 
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData((prevState) => ({...prevState, [e.target.name]: e.target.value}))
@@ -32,12 +49,24 @@ function Page() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setError(null);
     setSuccess(null);
 
-    console.log('Form Data:', formData)
+    const validation = signupSchema.safeParse(formData);
 
-    
+    if (!validation.success) {
+      console.log('Form is invalid:', validation.error.errors);
+
+      const errorMap: { [key: string]: string} = validation.error.errors.reduce((acc: any, error: any) => {
+        acc[error.path[0]] = error.message;
+        return acc;
+      }, {})
+
+      setErrors(errorMap);
+      return;
+    }
+
+    setErrors({});
+
     const res = await fetch('/api/auth/signup', {
       method: 'POST',
       headers: {
@@ -56,7 +85,10 @@ function Page() {
       setSuccess(data.message);
       router.push('/login');
     } else {
-      setError(data.error);
+      if (data.message === 'User already exists') {
+        setErrors(prev => ({...prev, userExists: data.message}))
+      }
+      console.error('Network error:', data);
     }
   }
 
@@ -75,32 +107,37 @@ function Page() {
         <form className='flex flex-col gap-3 mr-10' onSubmit={handleSubmit}>
           <h2 className='font-sans mt-3 text-2xl'>Sign up</h2>
           <hr className='mt-2 mb-5 border '/>
-          {error && <p className='text-red-600'>{error}</p>}
           {success && <p className='text-green-600'>{success}</p>}
 
           <div className="flex flex-col gap-3">
+            {errors.userExists && <p className='text-red-500 text-sm ml-2'>{errors.userExists}</p>}
+            {errors.email && <p className='text-red-500 text-sm mt-0 ml-2'>{errors.email}</p>}
             <TextField
               label="Email address"
               onChange={handleChange}
               name="email"
              />
+            {errors.password && <p className='text-red-500 text-sm mt-0 ml-2'>{errors.password}</p>}
             <TextField
               label="Password"
               type="password"
               name='password'
               onChange={handleChange}
              />
+            {errors.confirmPassword && <p className='text-red-500 text-sm mt-0 ml-2'>{errors.confirmPassword}</p>}
             <TextField
               label="Confirm Password"
               type="password"
-              name=''
+              name='confirmPassword'
               onChange={handleChange}
              />
+            {errors.location && <p className='text-red-500 text-sm mt-0 ml-2'>{errors.location}</p>}
             <TextField
               label="Location"
               name='location'
               onChange={handleChange}
              />
+             {errors.phoneNumber && <p className='text-red-500 text-sm mt-0 ml-2'>{errors.phoneNumber}</p>}
             <TextField
               label="Phone number"
               name='phoneNumber'
@@ -110,14 +147,18 @@ function Page() {
           <FormControlLabel
             label="I accept the Terms and Conditions"
             control={
-                <OrangeCheckbox {...label} />
+                <OrangeCheckbox
+                  checked={termsAccepted}
+                  onChange={() => setTermsAccepted(prev => !prev)}
+                  {...label} />
             }
             className='font-sans'
           />
 
           <div className="mt-5 flex justify-center">
             <Button variant="contained" 
-            type='submit'
+              type='submit'
+              disabled={!termsAccepted}
               sx={{
                 backgroundColor: '#FF8100',
                 width: '100%',
