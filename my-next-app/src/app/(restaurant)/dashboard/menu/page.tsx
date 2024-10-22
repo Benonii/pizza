d90c78072c384e8893352f6efc6e8b2b60bb7ea7
BENONI ESCKINDER <you@example.com>
2024-10-22 23:23:58 +0300

    Input validation for /dashboard/menu complete

--- my-next-app/src/app/(restaurant)/dashboard/menu/page.tsx
+++ my-next-app/src/app/(restaurant)/dashboard/menu/page.tsx
@@ -7,12 +7,12 @@ import OrangeCheckbox from '@/components/OrangeCheckbox';
 import UploadButton from '@/components/UploadFile';
 // import Button from '@mui/material/Button';
 import PizzaUploadedModal from '@/components/PizzaUploadedModal';
-
+import { z } from 'zod';
 
 function Page() {
   const [ formData, setFormData ] = useState({
     name: '',
-    price: '',
+    price: 150,
   });
 
   const availableToppings = ['Mozzarella', 'Tomato', 'Bell Peppers', 'Onions', 'Olives']
@@ -20,11 +20,15 @@ function Page() {
 
   const [ success, setSuccess ] = useState<string | null>(null);
   const [ error, setError ] = useState<string | null>(null);
+  const [ errors, setErrors ] = useState<{[key: string]: string}>({});
   const [restaurantId, setRestaurantId] = useState<string | null>(null);
 
   const label = { inputProps: { 'aria-label': 'Toppings' } };
 
-
+  const menuSchema = z.object({
+    name: z.string().min(2, 'Pizza name too small'),
+    price: z.number(),
+  })
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -33,6 +37,12 @@ function Page() {
     }
   }, []);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // let value: string | number;
+    // if (e.target.name === 'price') {
+    //   value = Number(e.target.value);
+    // } else {
+    //   value = e.target.value
+    // }
     setFormData((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
   }
 
@@ -47,6 +57,20 @@ function Page() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    
+    const validation = menuSchema.safeParse(formData);
+
+    if (!validation.success) {
+      console.log('Form is invalid:', validation.error.errors);
+
+      const errorMap: { [key: string]: string} = validation.error.errors.reduce((acc: any, error: any) => {
+        acc[error.path[0]] = error.message;
+        return acc;
+      }, {})
+
+      setErrors(errorMap);
+      return;
+    }
     setSuccess(null);
     setError(null);
 
@@ -62,7 +86,7 @@ function Page() {
     if (res.ok) {
       if (data.success) {
         setSuccess('Pizza Created Successfully');
-        setFormData({ name: '', price: '' });
+        setFormData({ name: '', price: 150 });
         setToppings([]);
       } else {
         setError("Failed to create Pizza");
@@ -76,9 +100,11 @@ function Page() {
     <form onSubmit={handleSubmit} className='flex flex-col items-center w-screen h-screen bg-white'>
       <div>
         <h1 className='font-sans text-2xl text-gray6 text-center mt-10 mb-5'>Add Menu</h1>
+        {errors.name && <p className='text-red-500 text-sm ml-2'>{errors.name}</p>}
         <TextField
           label="Name"
           name="name"
+          value={formData.name}
           onChange={handleChange}
           sx={{
             width: '50vw',
@@ -106,17 +132,18 @@ function Page() {
           
         </div>
       </div>
-      
+      {errors.price && <p className='text-red-500 text-sm ml-2'>{errors.price}</p>}
       <TextField
         label="Price"
         name="price"
+        value={formData.price}
         onChange={handleChange}
         sx={{
           width: '50vw',
           minWidth: 250,
         }}
         />
-        <div className="w-[60vw] max-w-72 mt-5">
+        <div className="w-[60vw] max-w-72 mt-5" aria-disabled={!errors}>
           <UploadButton />
         </div>
         <div className='flex justify-center items-center w-full mt-5'>
